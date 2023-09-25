68f6bc305b95e749bc5b14315479e17ca86536c2
Isaias-Tortorele <isaiastortorele23@gmail.com>
2023-09-25 16:10:13 -0300

    finished project

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,42 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Novo homem de ferro',
+    description:
+      'lor ajsndoasuifdis dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf',
+    photoCover:
+      'https://i0.wp.com/www.portaldonerd.com.br/wp-content/uploads/2018/04/capa-1.jpg',
+  },
+  {
+    id: '2',
+    title: 'Homem de ferro morreu?',
+    description:
+      'lor ajsndoasuifdis dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf!',
+    photoCover:
+      'https://i0.wp.com/www.portaldonerd.com.br/wp-content/uploads/2018/04/capa-1.jpg',
+  },
+  {
+    id: '3',
+    title: 'Homem de ferro morreu?',
+    description:
+      'lor ajsndoasuifdis dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf!',
+    photoCover:
+      'https://i0.wp.com/www.portaldonerd.com.br/wp-content/uploads/2018/04/capa-1.jpg',
+  },
+  {
+    id: '4',
+    title: 'Homem de ferro morreu?',
+    description:
+      'lor ajsndoasuifdis dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf!',
+    photoCover:
+      'https://i0.wp.com/www.portaldonerd.com.br/wp-content/uploads/2018/04/capa-1.jpg',
+  },
+  {
+    id: '5',
+    title: 'Homem de ferro morreu?',
+    description:
+      'lor ajsndoasuifdis dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf dshfisdjn nsidnuf lor ajsndoasuifdis dshfisdjn nsidnuf!',
+    photoCover:
+      'https://i0.wp.com/www.portaldonerd.com.br/wp-content/uploads/2018/04/capa-1.jpg',
+  },
+];
